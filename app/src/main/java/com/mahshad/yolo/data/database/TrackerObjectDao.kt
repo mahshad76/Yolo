5e7dd8ade77080de74cd23c622c7706603f39b38
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2026-02-01 18:43:39 -0500

    Dao is done

--- app/src/main/java/com/mahshad/yolo/data/database/TrackerObjectDao.kt
+++ app/src/main/java/com/mahshad/yolo/data/database/TrackerObjectDao.kt
@@ -1,10 +1,27 @@
 package com.mahshad.yolo.data.database
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.mahshad.yolo.data.database.model.TrackerObjectEntity
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface TrackerObjectDao {
     @Query("SELECT * FROM tracker_objects")
     fun getAll(): Flow<List<TrackerObjectEntity>>
+
+    @Update(TrackerObjectEntity::class)
+    suspend fun partialUpdate(trackerObjectEntity: TrackerObjectEntity): Int
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(trackerObject: TrackerObjectEntity): Long
+
+    //    delete the tracker which haven't been seen for a period of time, indicating the back and
+    //    forth movements of the camera
+    @Delete
+    suspend fun delete(trackerObject: TrackerObjectEntity): Int
 }
