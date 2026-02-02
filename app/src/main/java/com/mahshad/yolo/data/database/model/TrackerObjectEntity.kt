47e7b3f51d507931337b5a41285cd56e9795ad70
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2026-02-01 19:12:32 -0500

    the entity has changed

--- app/src/main/java/com/mahshad/yolo/data/database/model/TrackerObjectEntity.kt
+++ app/src/main/java/com/mahshad/yolo/data/database/model/TrackerObjectEntity.kt
@@ -7,7 +7,10 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "tracker_objects")
 data class TrackerObjectEntity(
     @PrimaryKey val label: String,
-    @ColumnInfo("bounding_box") val boundingBox: List<Float>,
+    @ColumnInfo(name = "bbox_left") val left: Float,
+    @ColumnInfo(name = "bbox_top") val top: Float,
+    @ColumnInfo(name = "bbox_right") val right: Float,
+    @ColumnInfo(name = "bbox_bottom") val bottom: Float,
     @ColumnInfo("notice_time") val noticeTime: Double,
     @ColumnInfo("confidence_level") val confidenceLevel: Float,
     @ColumnInfo("object_count") val count: Int = 0
